9b46043ee3c49f971ce9dd08c5d1078f5d1b9837
dominthomas <dthomas@donbibi.localdomain>
2020-03-02 01:30:55 +0000

    Using 'model.fit()' instead of 'model.fit_generator()'

--- 3D_data_generator.py
+++ 3D_data_generator.py
@@ -128,11 +128,11 @@ params = {'dim': (176, 256, 256),
           'shuffle': True}
 training_generator = DataGenerator(partition['train'], labels, **params)
 validation_generator = DataGenerator(partition['validation'], labels, **params)
-model.fit_generator(generator=training_generator,
-                    validation_generator=validation_generator,
-                    use_multiprocessing=True,
-                    workers=5,
-                    epochs=200)
+model.fit(generator=training_generator,
+          validation_generator=validation_generator,
+          use_multiprocessing=True,
+          workers=5,
+          epochs=200)
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
 ad_test_files = os.listdir("/home/k1651915/ADNI/3D/AD/")
