b7bfabf7edf8d540b367e1cbaf528ed0d829b4f9
dominthomas <dthomas@donbibi.localdomain>
2020-03-02 19:10:20 +0000

    automated commit

--- tf_data_generator/3D_tf_data_generator.py
+++ tf_data_generator/3D_tf_data_generator.py
@@ -48,7 +48,7 @@ print(len(labels))
 
 
 def load_image(file, label):
-    nifti = np.asarray(nibabel.load(file.getitem).get_fdata())
+    nifti = np.asarray(nibabel.load(tf.cast(file, np.float32)).get_fdata())
 
     xs, ys, zs = np.where(nifti != 0)
     nifti = nifti[min(xs):max(xs) + 1, min(ys):max(ys) + 1, min(zs):max(zs) + 1]
