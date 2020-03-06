beff459f26949258bb27eb01db8135a6ab4ee535
dominthomas <dthomas@donbibi.localdomain>
2020-03-06 15:10:29 +0000

    Automated Commit

--- tf_data_generator/3D_tf_data_generator.py
+++ tf_data_generator/3D_tf_data_generator.py
@@ -72,8 +72,9 @@ def load_image_wrapper(file, labels):
 
 dataset = tf.data.Dataset.from_tensor_slices((train, labels))
 dataset = dataset.map(load_image_wrapper, num_parallel_calls=6)
-dataset = dataset.batch(6, drop_remainder=True).repeat(50)
+dataset = dataset.batch(6, drop_remainder=True)
 dataset = dataset.prefetch(buffer_size=1)
+dataset = dataset.repeat(50)
 iterator = iter(dataset)
 batch_images, batch_labels = iterator.get_next()
 
