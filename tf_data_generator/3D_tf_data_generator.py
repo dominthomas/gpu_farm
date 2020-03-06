63d48405bc8f131823af85a40630adb38de96149
dominthomas <dthomas@donbibi.localdomain>
2020-03-06 17:06:27 +0000

    Automated Commit

--- tf_data_generator/3D_tf_data_generator.py
+++ tf_data_generator/3D_tf_data_generator.py
@@ -71,12 +71,12 @@ def load_image_wrapper(file, labels):
 
 
 dataset = tf.data.Dataset.from_tensor_slices((train, labels))
-dataset = dataset.repeat(50)
+dataset = dataset.repeat()
 dataset = dataset.map(load_image_wrapper, num_parallel_calls=6)
 dataset = dataset.batch(6, drop_remainder=True)
 dataset = dataset.prefetch(buffer_size=4)
 iterator = iter(dataset)
-batch = iterator.get_next()
+batch_images, batch_labels = iterator.get_next()
 
 
 ########################################################################################
@@ -141,7 +141,7 @@ model.compile(loss=tf.keras.losses.binary_crossentropy,
 
 ########################################################################################
 ########################################################################################
-model.fit(batch, steps_per_epoch=92, epochs=50)
+model.fit(batch_images, batch_labels, steps_per_epoch=92, epochs=50)
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
 test_size = 5
