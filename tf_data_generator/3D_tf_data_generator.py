7298babd8ecaddbde044fba1c8728dad4007a41e
dominthomas <dthomas@donbibi.localdomain>
2020-03-06 13:53:48 +0000

    Automated Commit

--- tf_data_generator/3D_tf_data_generator.py
+++ tf_data_generator/3D_tf_data_generator.py
@@ -67,7 +67,7 @@ def load_image(file, label):
 
 @tf.autograph.experimental.do_not_convert
 def load_image_wrapper(file, labels):
-    return tf.py_function(load_image, [file, labels], [tf.float32, tf.float32])
+    return tf.py_function(load_image, [file, labels], [tf.float64, tf.float64])
 
 
 dataset = tf.data.Dataset.from_tensor_slices((train, labels))
