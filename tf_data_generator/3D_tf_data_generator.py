b45991a5f9af69ebffaac73ba8319378db72ad50
dominthomas <dthomas@donbibi.localdomain>
2020-03-06 14:00:19 +0000

    Automated Commit

--- tf_data_generator/3D_tf_data_generator.py
+++ tf_data_generator/3D_tf_data_generator.py
@@ -140,7 +140,7 @@ model.compile(loss=tf.keras.losses.categorical_crossentropy,
 
 ########################################################################################
 ########################################################################################
-model.fit(batch_of_images[0], batch_of_images[1], steps_per_epoch=10, epochs=50)
+model.fit(batch_of_images[0], batch_of_images[1], steps_per_epoch=90, epochs=50)
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
 test_size = 5
