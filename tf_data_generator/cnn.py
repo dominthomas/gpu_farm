51572d6aebc5c7ede9abf7be4cce0910e08e6da2
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 17:19:10 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -91,7 +91,7 @@ def load_image_wrapper(file, label):
 
 dataset = tf.data.Dataset.from_tensor_slices((train, labels))
 dataset = dataset.map(load_image_wrapper, num_parallel_calls=12)
-dataset = dataset.batch(10, drop_remainder=True).repeat()
+dataset = dataset.batch(19, drop_remainder=True).repeat()
 # dataset = dataset.prefetch(buffer_size=2)
 iterator = iter(dataset)
 image_batch, label_batch = iterator.get_next()
@@ -154,7 +154,7 @@ model.compile(loss=tf.keras.losses.binary_crossentropy,
               optimizer=tf.keras.optimizers.Adagrad(0.01),
               metrics=['accuracy'])
 ########################################################################################
-model.fit(x=image_batch, y=label_batch, steps_per_epoch=92, epochs=50, validation_data=(v, v_labels))
+model.fit(x=image_batch, y=label_batch, steps_per_epoch=55, epochs=50, validation_data=(v, v_labels))
 ########################################################################################
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
