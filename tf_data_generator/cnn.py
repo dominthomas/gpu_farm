7b450ae6b5b71f0d6dbe959eed1763f438a8dbb1
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 16:33:20 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -62,7 +62,7 @@ os.chdir("/home/k1651915/OASIS/3D/all/")
 """Get Validation Data"""
 v_ad = ad_files[0:5]
 v_cn = cn_files[0:5]
-v_labels = np.concatenate((np.ones(5), np.zeros(5)), axis=None)
+v_labels = np.concatenate((np.ones(6), np.zeros(6)), axis=None)
 v_ad = get_images(v_ad)
 v_cn = get_images(v_cn)
 v = np.asarray(v_ad + v_cn)
