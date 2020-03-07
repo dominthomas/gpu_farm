6c3d4077fd42ba41da9994aec5dd4d34daa7e594
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 20:55:21 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -83,7 +83,7 @@ def load_image(file, label):
 
 @tf.autograph.experimental.do_not_convert
 def load_image_wrapper(file, label):
-    result_tensors = f.py_function(load_image, [file, label], [tf.float64, tf.float64])
+    result_tensors = tf.py_function(load_image, [file, label], [tf.float64, tf.float64])
     result_tensors.set_shape([None, None, None, None])
     return result_tensors
 
