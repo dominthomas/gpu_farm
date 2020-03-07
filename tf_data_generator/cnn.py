3b8c4cf806a9a2161fc22f5088d45d8953076744
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 02:35:13 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -15,9 +15,10 @@ import gc
 """@author Domin Thomas"""
 """Make sure that the working directory for this python script is in the '/home/k1651915/OASIS/3D/all/' , 
 or in the ADNI 3D/all/ subdirectory depending on the training dataset """
-tf.compat.v1.enable_eager_execution()
-sess = tf.compat.v1.Session()
-tf.compat.v1.keras.backend.set_session(sess)
+
+# tf.compat.v1.reset_default_graph()
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
 
 """Configure GPUs to prevent OOM errors"""
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -43,11 +44,11 @@ cn_train = cn_files[0:276]
 
 """Shuffle Train data and Train labels"""
 train = ad_train + cn_train
-train_labels = np.concatenate((np.ones(len(ad_train)), np.zeros(len(cn_train))), axis=None)
+labels = np.concatenate((np.ones(len(ad_train)), np.zeros(len(cn_train))), axis=None)
 random.Random(129).shuffle(train)
-random.Random(129).shuffle(train_labels)
+random.Random(129).shuffle(labels)
 print(len(train))
-print(len(train_labels))
+print(len(labels))
 
 """Change working directory to OASIS/3D/all/"""
 os.chdir("/home/k1651915/OASIS/3D/all/")
@@ -63,97 +64,83 @@ def load_image(file, label):
     nifti = nifti[0:100, 0:100, 0:100]
     nifti = np.reshape(nifti, (100, 100, 100, 1))
     nifti = tf.convert_to_tensor(nifti, tf.float64)
-    return nifti, tf.cast(label, tf.float64)
+    return nifti, tf.cast(label, tf.int32)
 
 
 @tf.autograph.experimental.do_not_convert
-def load_image_wrapper(file, train_label):
-    return tf.py_function(load_image, [file, train_labels], [tf.float64, tf.float64])
+def load_image_wrapper(file, labels):
+    return tf.py_function(load_image, [file, labels], [tf.float64, tf.int32])
 
 
-dataset = tf.data.Dataset.from_tensor_slices((train, train_labels))
+dataset = tf.data.Dataset.from_tensor_slices((train, labels))
 dataset = dataset.map(load_image_wrapper, num_parallel_calls=12)
 dataset = dataset.batch(12, drop_remainder=True).repeat()
 dataset = dataset.prefetch(buffer_size=2)
 iterator = iter(dataset)
 batch = iterator.get_next()
 
+
 ########################################################################################
 with tf.device("/cpu:0"):
-    img = tf.keras.backend.placeholder(dtype=tf.float64, shape=(12, 100, 100, 100, 1))
     with tf.device("/gpu:0"):
-        x = Conv3D(64,
-                   data_format='channels_last',
-                   kernel_size=(7, 7, 7),
-                   strides=(2, 2, 2),
-                   padding='valid',
-                   activation='relu')(img)
+        model = tf.keras.Sequential()
 
-        with tf.device("/gpu:1"):
-            x = Conv3D(64,
-                       kernel_size=(3, 3, 3),
-                       padding='valid',
-                       activation='relu')(x)
+        model.add(Conv3D(64,
+                         input_shape=(100, 100, 100, 1),
+                         data_format='channels_last',
+                         kernel_size=(7, 7, 7),
+                         strides=(2, 2, 2),
+                         padding='valid',
+                         activation='relu'))
 
-        with tf.device("/gpu:2"):
-            x = Conv3D(128,
-                       kernel_size=(3, 3, 3),
-                       padding='valid',
-                       activation='relu')(x)
+    with tf.device("/gpu:1"):
+        model.add(Conv3D(64,
+                         kernel_size=(3, 3, 3),
+                         padding='valid',
+                         activation='relu'))
 
-            x = MaxPooling3D(pool_size=(2, 2, 2),
-                             padding='valid')(x)
+    with tf.device("/gpu:2"):
+        model.add(Conv3D(128,
+                         kernel_size=(3, 3, 3),
+                         padding='valid',
+                         activation='relu'))
 
-        with tf.device("/gpu:3"):
-            x = Conv3D(128,
-                       kernel_size=(3, 3, 3),
-                       padding='valid',
-                       activation='relu')(x)
+        model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                               padding='valid'))
 
-            x = MaxPooling3D(pool_size=(2, 2, 2),
-                             padding='valid')(x)
+    with tf.device("/gpu:3"):
+        model.add(Conv3D(128,
+                         kernel_size=(3, 3, 3),
+                         padding='valid',
+                         activation='relu'))
 
-        with tf.device("/gpu:4"):
-            x = Conv3D(128,
-                       kernel_size=(3, 3, 3),
-                       padding='valid',
-                       activation='relu')(x)
+        model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                               padding='valid'))
 
-            x = MaxPooling3D(pool_size=(2, 2, 2),
-                             padding='valid')(x)
+    with tf.device("/gpu:4"):
+        model.add(Conv3D(128,
+                         kernel_size=(3, 3, 3),
+                         padding='valid',
+                         activation='relu'))
 
-            x = Flatten()(x)
+        model.add(MaxPooling3D(pool_size=(2, 2, 2),
+                               padding='valid'))
 
-            x = Dense(256, activation='relu')(x)
-            x = Dropout(0.7)(x)
-            x = Dense(256, activation='relu')(x)
-            x = Dropout(0.7)(x)
-            preds = Dense(2, activation='softmax')(x)
+        model.add(Flatten())
 
-labels = tf.keras.backend.placeholder(dtype=tf.float64, shape=(6, None))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(256, activation='relu'))
+        model.add(Dropout(0.7))
+        model.add(Dense(1, activation='sigmoid'))
 
-loss = tf.math.reduce_mean(tf.keras.losses.SparseCategoricalCrossentropy(labels, preds))
-train_step = tf.compat.v1.train.AdagradOptimizer(0.001).minimize(loss)
 
-sess.run(tf.global_variables_initializer())
-
+model.compile(loss=tf.keras.losses.binary_crossentropy,
+              optimizer=tf.keras.optimizers.Adagrad(0.01),
+              metrics=['accuracy'])
 ########################################################################################
-epochs = 5
-with sess.as_default():
-    for i in range(epochs):
-        sess.run(iterator)
-
-        try:
-            while True:
-                image_batch = sess.run(batch)
-                train_step.run(image_batch[0], image_batch[1])
-
-        except tf.errors.OutOfRangeError:
-            print('End of epoch...')
+model.fit(batch, steps_per_epoch=92, epochs=50)
 ########################################################################################
-
-
-# model.fit(batch_images, batch_labels, steps_per_epoch=92, epochs=50)
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
 test_size = 5
@@ -190,8 +177,8 @@ cn_test_labels = tf.keras.utils.to_categorical(np.zeros(test_size), 2)
 # ad_test_labels = np.ones(test_size), 2
 # cn_test_labels = np.zeros(test_size), 2
 
-evaluation_ad = preds.evaluate(ad_test, ad_test_labels, verbose=0)
-evaluation_cn = preds.evaluate(cn_test, cn_test_labels, verbose=0)
+evaluation_ad = model.evaluate(ad_test, ad_test_labels, verbose=0)
+evaluation_cn = model.evaluate(cn_test, cn_test_labels, verbose=0)
 
 print("AD: ", evaluation_ad[1])
 print("CN: ", evaluation_cn[1])
