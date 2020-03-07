a6cb9e1b0bddabe53e626ba82d339398185576bf
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 17:08:03 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -93,15 +93,29 @@ dataset = tf.data.Dataset.from_tensor_slices((train, labels))
 dataset = dataset.map(load_image_wrapper, num_parallel_calls=12)
 dataset = dataset.batch(6, drop_remainder=True).repeat()
 dataset = dataset.prefetch(buffer_size=2)
-#iterator = iter(dataset)
+iterator = iter(dataset)
+
+count = 0
 
 
-def get_batch():
-    for i in dataset:
-        return i
+def get_batch(curCount):
+    global iterator
+    global count
+    global dataset
+    if curCount == 91:
+        count = 0
+        curCount = 0
+
+        dataset = tf.data.Dataset.from_tensor_slices((train, labels))
+        dataset = dataset.map(load_image_wrapper, num_parallel_calls=12)
+        dataset = dataset.batch(6, drop_remainder=True).repeat()
+        dataset = dataset.prefetch(buffer_size=2)
+        iterator = iter(dataset)
+    count = curCount + 1
+    return iterator.get_next()
 
 
-image_batch, label_batch = get_batch()
+image_batch, label_batch = get_batch(count)
 
 ########################################################################################
 with tf.device("/cpu:0"):
