7965ff2683d7fd62246ee9fd769cdf6df1d20cb7
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 03:06:04 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -78,6 +78,7 @@ dataset = dataset.batch(12, drop_remainder=True).repeat()
 dataset = dataset.prefetch(buffer_size=2)
 iterator = iter(dataset)
 batch = iterator.get_next()
+print(batch)
 
 
 ########################################################################################
@@ -139,7 +140,7 @@ model.compile(loss=tf.keras.losses.binary_crossentropy,
               optimizer=tf.keras.optimizers.Adagrad(0.01),
               metrics=['accuracy'])
 ########################################################################################
-model.fit_generator(batch, steps_per_epoch=92, epochs=50)
+model.fit(batch, steps_per_epoch=92, epochs=50)
 ########################################################################################
 
 """Load test data from ADNI, 50 AD & 50 CN MRIs"""
