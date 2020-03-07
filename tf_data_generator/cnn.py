e05db756b976bc44d35a7b5f9c3df6639f11d50a
dominthomas <dthomas@donbibi.localdomain>
2020-03-07 15:37:07 +0000

    Automated Commit

--- tf_data_generator/cnn.py
+++ tf_data_generator/cnn.py
@@ -59,10 +59,10 @@ def load_image(file, label):
     xs, ys, zs = np.where(nifti != 0)
     nifti = nifti[min(xs):max(xs) + 1, min(ys):max(ys) + 1, min(zs):max(zs) + 1]
     # TODO revert
-    # nifti = nifti[0:100, 0:100, 0:100]
-    nifti = nifti[0:2, 0:2, 0:2]
-    # nifti = np.reshape(nifti, (1, 100, 100, 100))
-    nifti = np.reshape(nifti, (2, 2, 2, 1))
+    nifti = nifti[0:100, 0:100, 0:100]
+    # nifti = nifti[0:2, 0:2, 0:2]
+    nifti = np.reshape(nifti, (1, 100, 100, 100))
+    #$ nifti = np.reshape(nifti, (2, 2, 2, 1))
     # return {file.numpy().decode('utf-8'): nifti}
     return nifti, label
 
